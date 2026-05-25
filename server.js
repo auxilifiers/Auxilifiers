6efc88946c615b04ae7f8f75b75957eaa20e9d4c
Arham <arham@auxilifiers.com>
2026-05-26 00:46:01 +0500

    Force Next.js production mode in server.js to fix resource exhaustion and CSS loading

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const { createServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
 
-const dev = process.env.NODE_ENV !== 'production';
+const dev = false;
 const hostname = '0.0.0.0';
 const port = process.env.PORT || 3000;
 
